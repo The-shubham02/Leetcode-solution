cac003799776d0ecc4de54aad261d337af15bfce
Shubham kumar pandey <skpofficial1130@gmail.com>
2026-09-04 23:37:15 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (60.41%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,21 +1,18 @@
-class Solve {
-
-    int fun(int i, int end, int[] nums, int[] dp) {
-
-        if (i > end)
-            return 0;
-
-        if (dp[i] != -1)
-            return dp[i];
-
-        int pick = nums[i] + fun(i + 2, end, nums, dp);
-        int notPick = fun(i + 1, end, nums, dp);
-
-        return dp[i] = Math.max(pick, notPick);
-    }
-}
-
 class Solution {
+
+    int solve(int start, int end, int[] nums) {
+
+        int[] dp = new int[end + 3];
+
+        for (int i = end; i >= start; i--) {
+            int pick = nums[i] + dp[i + 2];
+            int notPick = dp[i + 1];
+
+            dp[i] = Math.max(pick, notPick);
+        }
+
+        return dp[start];
+    }
 
     public int rob(int[] nums) {
 
@@ -24,17 +21,9 @@ class Solution {
         if (n == 1)
             return nums[0];
 
-        Solve s = new Solve();
-
-        int[] dp1 = new int[n];
-        Arrays.fill(dp1, -1);
-
-        int[] dp2 = new int[n];
-        Arrays.fill(dp2, -1);
-
-        int ans1 = s.fun(0, n - 2, nums, dp1);
-        int ans2 = s.fun(1, n - 1, nums, dp2);
-
-        return Math.max(ans1, ans2);
+        return Math.max(
+                solve(0, n - 2, nums),
+                solve(1, n - 1, nums)
+        );
     }
 }
