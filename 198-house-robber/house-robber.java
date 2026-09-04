38f69428a66b7d073de66c5385ec1a31de57971a
Shubham kumar pandey <skpofficial1130@gmail.com>
2026-09-04 23:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (75.08%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,27 +1,26 @@
-import java.util.Arrays;
+
+class solve{
+    int fun(int i, int[] nums , int[] dp){
+        int n = nums.length;
+        if (i>=n) 
+        return 0;
+        if(dp[i] != -1){
+            return dp[i];
+        }
+            int c1 = nums[i]+ fun(i+2 , nums ,dp);
+            int c2 = fun(i+1, nums,dp);
+            return dp[i] = Math.max(c1,c2);
+  }
+
+}
 
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
-
-        return solve(nums, 0, dp);
-    }
-
-    public int solve(int[] nums, int i, int[] dp) {
-        if (i >= nums.length) {
-            return 0;
-        }
-
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        int take = nums[i] + solve(nums, i + 2, dp);
-        int notTake = solve(nums, i + 1, dp);
-
-        dp[i] = Math.max(take, notTake);
-
-        return dp[i];
+        int n = nums.length;
+        int [] dp = new int[n+2];
+    Arrays.fill(dp , -1);
+       solve s = new solve();
+       int answer = s.fun(0,nums, dp);
+        return answer;
     }
 }
